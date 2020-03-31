0dcfc6c995e7070523623a6afd4c14180c17348f
Pistrie <39904003+Pistrie@users.noreply.github.com>
2020-03-31 16:11:55 +0200

    update

--- src/ComicLibrary.java
+++ src/ComicLibrary.java
@@ -1,5 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ComicLibrary {
 
     public ComicLibrary() {
+        Connection conn = null;
+        try {
+            // get connection
+            String connectionUrl = "jdbc:mysql://localhost:3306/comics";
+            conn = DriverManager.getConnection(connectionUrl, "root", "rootpassword");
+//            // create statement
+//            Statement myStmt = conn.createStatement();
+//            // execute sql
+//            ResultSet myRs = myStmt.executeQuery("SELECT * FROM comics");
+//            // process result set
+//            while (myRs.next()) {
+//                System.out.println(myRs.getString("title") + ", "
+//                        + myRs.getString("publication_number") + ", "
+//                        + myRs.getString("isbn"));
+//            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,6 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            // get connection
-            String connectionUrl = "jdbc:mysql://localhost:3306/comics";
-            Connection conn = DriverManager.getConnection(connectionUrl, "root", "rootpassword");
-//            // create statement
-//            Statement myStmt = conn.createStatement();
-//            // execute sql
-//            ResultSet myRs = myStmt.executeQuery("SELECT * FROM comics");
-//            // process result set
-//            while (myRs.next()) {
-//                System.out.println(myRs.getString("title") + ", "
-//                        + myRs.getString("publication_number") + ", "
-//                        + myRs.getString("isbn"));
-//            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        ComicLibrary comicLibrary = new ComicLibrary();
     }
 }
