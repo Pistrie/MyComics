d21431913e847d96dd849dfd8ecdd62010012de6
Pistrie <39904003+Pistrie@users.noreply.github.com>
2020-04-01 12:53:32 +0200

    rework

--- src/CRUD/Read.java
+++ src/CRUD/Read.java
@@ -1,10 +1,66 @@
 package CRUD;
 
+import java.sql.*;
+
 public class Read {
-    String mysql = """
-            SELECT *
-            FROM comics
-            WHERE title = :title
-            OR title = null
-            """;
+    private Connection conn;
+    private String filter;
+
+    public Read(Connection conn, String filter) {
+        this.conn = conn;
+        this.filter = filter;
+        PreparedStatement myStmt = null;
+        ResultSet myRs = null;
+
+        try {
+            // prepare statement
+            myStmt = conn.prepareStatement("""
+                SELECT *
+                FROM comics
+                WHERE title = ?
+                OR ? IS NULL
+                """);
+
+            // set parameters
+            System.out.println("This is where the user would type the filter");
+            if (filter.equals("")) {
+                myStmt.setNull(1, Types.NVARCHAR);
+                myStmt.setNull(2, Types.NVARCHAR);
+            } else {
+                myStmt.setString(1, filter);
+                myStmt.setString(2, filter);
+            }
+
+            // execute sql query
+            myRs = myStmt.executeQuery();
+
+            // display result set
+            while (myRs.next()) {
+                System.out.println(myRs.getString("title"));
+                System.out.println(myRs.getString("publication_number"));
+                System.out.println(myRs.getString("genre"));
+                System.out.println(myRs.getString("publication_year"));
+                System.out.println(myRs.getString("publisher"));
+                System.out.println(myRs.getString("isbn"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (myRs != null) {
+                try {
+                    myRs.close();
+                } catch (SQLException e) { /* ignored */}
+            }
+            if (myStmt != null) {
+                try {
+                    myStmt.close();
+                } catch (SQLException e) { /* ignored */}
+            }
+            if (this.conn != null) {
+                try {
+                    this.conn.close();
+                } catch (SQLException e) { /* ignored */}
+            }
+        }
+    }
 }
--- src/ComicLibrary.java
+++ src/ComicLibrary.java
@@ -1,56 +1,28 @@
-import java.sql.*;
-import java.util.Scanner;
+import CRUD.Read;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ComicLibrary {
+    private static final String DB_DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "rootpassword";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/comics";
 
-    public ComicLibrary() {
-        Connection conn = null;
-        PreparedStatement myStmt = null;
-        ResultSet myRs = null;
-        Scanner reader = null;
+    private static Connection connection = null;
+    static{
         try {
-            // get connection
-            String connectionUrl = "jdbc:mysql://localhost:3306/comics";
-            conn = DriverManager.getConnection(connectionUrl, "root", "rootpassword");
-
-            // prepare statement
-            myStmt = conn.prepareStatement("""
-            SELECT *
-            FROM comics
-            WHERE title = ?
-            OR ? IS NULL
-            """);
-
-            // set parameters
-            System.out.print("Enter the name you want to filter on. Leave this empty if you don't want to filter: ");
-            reader = new Scanner(System.in);
-            String filter = reader.nextLine();
-            if (filter.equals("")) {
-                myStmt.setNull(1, Types.NVARCHAR);
-                myStmt.setNull(2, Types.NVARCHAR);
-            } else {
-                myStmt.setString(1, filter);
-                myStmt.setString(2, filter);
-            }
-
-            // execute sql query
-            myRs = myStmt.executeQuery();
-
-            // display result set
-            while (myRs.next()) {
-                System.out.println(myRs.getString("title"));
-                System.out.println(myRs.getString("publication_number"));
-                System.out.println(myRs.getString("genre"));
-                System.out.println(myRs.getString("publication_year"));
-                System.out.println(myRs.getString("publisher"));
-                System.out.println(myRs.getString("isbn"));
-            }
-        }
-        catch (Exception e) {
+            Class.forName(DB_DRIVER_CLASS);
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
     }
 
-    private void display(ResultSet myRs) {
+    Read read = new Read(connection, "Dirkjan");
+
+    public static Connection getConnection(){
+        return connection;
     }
 }
