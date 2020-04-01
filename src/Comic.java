4151e2c249d06d49334fd9a119a873230ebd1dcb
Pistrie <39904003+Pistrie@users.noreply.github.com>
2020-04-01 16:56:19 +0200

    update Comic.java

--- src/Comic.java
+++ src/Comic.java
@@ -1,2 +1,17 @@
 public class Comic {
+    private String title;
+    private int publication_number;
+    private String genre;
+    private int publication_year;
+    private String publisher;
+    private int isbn;
+
+    public Comic(String title, int publication_number, String genre, int publication_year, String publisher, int isbn) {
+        this.title = title;
+        this.publication_number = publication_number;
+        this.genre = genre;
+        this.publication_year = publication_year;
+        this.publisher = publisher;
+        this.isbn = isbn;
+    }
 }
